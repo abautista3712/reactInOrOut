052c87983a3a7aea322a4fe5b25a1d887c68d335
Abelard Bautista <abelardbautista@gmail.com>
2022-01-21 18:20:34 -0800

    Added background for landing page.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # reactInOrOut
+
+Chopping Board photo on Landing Page by <a href="https://unsplash.com/@kate5oh3?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Katie Smith</a> on <a href="https://unsplash.com/s/photos/cooking?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
---
+++ src/pages/Landing.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+// import Search from "./Search";
+
+const Landing = (props) => {
+  return (
+    <div className="">
+      <img
+        src="./assets/images/original/choppingBoard.JPG"
+        alt="Chopping Board with Food"
+        className="backgroundImg"
+      />
+    </div>
+  );
+};
+
+export default Landing;
